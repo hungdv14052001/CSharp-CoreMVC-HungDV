809aa65df8616a01d2a1da320230fc980296656b
hungdv14052001 <hung14052001xyz@gmail.com>
2022-07-01 14:42:12 +0700

    Completed

---
+++ CSharp-CoreMVC-HungDV/Data/CSharp_CoreMVC_HungDVContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CSharp_CoreMVC_HungDV.Models;
+
+namespace CSharp_CoreMVC_HungDV.Data
+{
+    public class CSharp_CoreMVC_HungDVContext : DbContext
+    {
+        public CSharp_CoreMVC_HungDVContext (DbContextOptions<CSharp_CoreMVC_HungDVContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CSharp_CoreMVC_HungDV.Models.Movie> Movie { get; set; }
+    }
+}
